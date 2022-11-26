d822c3bd7d27021cf508c3f072d662b7d5fab40c
NVLinh2507 <linhyeuthao1201@gmail.com>
2022-11-26 12:15:49 +0700

    Linhdish

---
+++ app/src/main/java/hieuntph22081/fpoly/goidiclient/ManHinhChao.java
@@ -0,0 +1,28 @@
+package hieuntph22081.fpoly.goidiclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class ManHinhChao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+        ImageView ivlogo = findViewById(R.id.ivlogo);
+
+        Glide.with(this).load(R.drawable.logo).into(ivlogo);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ManHinhChao.this, LoginActivity.class));
+            }
+        },3500);
+    }
+}
